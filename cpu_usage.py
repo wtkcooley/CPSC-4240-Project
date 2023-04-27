55c203956c8166cf23bf69e8024b3bf3fefa9f59
wcooley <wcooley@visiostack.com>
2023-04-27 15:53:54 -0400

    adds cpu by process & simple tool code

--- cpu_usage.py
+++ cpu_usage.py
@@ -1,4 +1,8 @@
 import psutil
 
-def get_cpu_usage(time=4):
+def get_cpu_usage(time=1):
     return psutil.cpu_percent(time)
+
+def get_cpu_usage_by_pid(pid, time=1):
+    p = psutil.Process(pid)
+    return p.cpu_percent(time)
--- tool.py
+++ tool.py
@@ -1,3 +1,22 @@
+import psutil
 import cpu_usage
 
 print('The CPU usage is: ', cpu_usage.get_cpu_usage())
+
+def main():
+    import psutil # for the linux process data and cpu usage
+
+    print(f"Process Name\tPID\tCPU Usage")
+    for proc in psutil.process_iter():
+        try:
+            # Get process name & pid from process object.
+            processName = proc.name()
+            processID = proc.pid
+            if cpu_usage.get_cpu_usage_by_pid(processID) > 0.0:
+                print(f"{processName}\t{processID}\t{cpu_usage.get_cpu_usage_by_pid(processID)}")
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
+if __name__ == '__main__':
+   main()
